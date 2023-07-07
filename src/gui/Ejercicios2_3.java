1ddd4a4361bc91569be58a28bcd99e0c7d8060dc
Enrique <Enrique@DESKTOP-Q74D7LQ>
2023-07-07 09:40:43 +0200

    Commit final

--- src/gui/Ejercicios2_3.java
+++ src/gui/Ejercicios2_3.java
@@ -13,10 +13,9 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPasswordField;
-import javax.swing.JSpinner;
-import javax.swing.JEditorPane;
-import javax.swing.JTextPane;
-import javax.swing.JDesktopPane;
+import javax.swing.event.CaretListener;
+import javax.swing.event.CaretEvent;
+import java.awt.Color;
 
 public class Ejercicios2_3 extends JFrame {
 
@@ -26,6 +25,7 @@ public class Ejercicios2_3 extends JFrame {
 	private final String PASSWORD = "1234";
 	private JPasswordField password;
 	private int cont = 0;
+	private JButton btnNewButton;
 	/**
 	 * Launch the application.
 	 */
@@ -48,33 +48,51 @@ public class Ejercicios2_3 extends JFrame {
 	public Ejercicios2_3() {
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 339, 191);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
+		
+		
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][][][][196.00,grow][grow][]", "[][][][][][][][]"));
+		contentPane.setLayout(new MigLayout("", "[][][][196.00,grow][45.00]", "[][][][][][]"));
 
 		JLabel lblNewLabel = new JLabel("Usuario:");
-		contentPane.add(lblNewLabel, "cell 2 1");
+		contentPane.add(lblNewLabel, "cell 1 1");
 
 		txtUsu = new JTextField();
-		contentPane.add(txtUsu, "cell 4 1,growx");
+		txtUsu.addCaretListener(new CaretListener() {
+			public void caretUpdate(CaretEvent e) {
+				if (txtUsu.getText()==null || txtUsu.getText().isBlank()) {
+					btnNewButton.setEnabled(false);
+				} else {
+					btnNewButton.setEnabled(true);
+				}
+			}
+		});
+		contentPane.add(txtUsu, "cell 3 1,growx");
 		txtUsu.setColumns(10);
 
 		JLabel lblNewLabel_1 = new JLabel("Contraseña:");
-		contentPane.add(lblNewLabel_1, "cell 2 3");
+		contentPane.add(lblNewLabel_1, "cell 1 3");
 
-		JButton btnNewButton = new JButton("Aceptar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		password = new JPasswordField();
+		contentPane.add(password, "cell 3 3,growx");
+		
+		btnNewButton = new JButton("Aceptar");
+		btnNewButton.setEnabled(false);
+		btnNewButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
 				login();
 			}
 		});
+		contentPane.add(btnNewButton, "cell 1 5 3 1,alignx center");
+		getRootPane().setDefaultButton(btnNewButton);
 
-		password = new JPasswordField();
-		contentPane.add(password, "cell 4 3,growx");
-		contentPane.add(btnNewButton, "cell 4 6,alignx right");
 	}
 
 	protected void login()
@@ -85,7 +103,6 @@ public class Ejercicios2_3 extends JFrame {
 			JOptionPane.showMessageDialog(null, "~ Login correcto, bienvenido al sistema ~");
 			System.exit(0);
 		}
-			//System.out.println("Bienvenido");
 		else
 		{
 			cont++;
